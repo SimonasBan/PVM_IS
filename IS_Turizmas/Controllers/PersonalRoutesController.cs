3a13e77613d02c9b9bf384e95ba0590a155f0398
jonasbuletas <76689162+jonasbuletas@users.noreply.github.com>
2021-05-15 21:27:17 +0300

    Added geolocation calculations

--- IS_Turizmas/Controllers/PersonalRoutesController.cs
+++ IS_Turizmas/Controllers/PersonalRoutesController.cs
@@ -120,17 +120,116 @@ namespace IS_Turizmas.Controllers
             //return View();
             return View();
         }
+        public class LocationDetails_IpApi
+        {
+            public string query { get; set; }
+            public string city { get; set; }
+            public string country { get; set; }
+            public string countryCode { get; set; }
+            public string isp { get; set; }
+            public double lat { get; set; }
+            public double lon { get; set; }
+            public string org { get; set; }
+            public string region { get; set; }
+            public string regionName { get; set; }
+            public string status { get; set; }
+            public string timezone { get; set; }
+            public string zip { get; set; }
+        }
+
+        public static double DistanceTo(double lat1, double lon1, double lat2, double lon2, char unit = 'K')
+        {
+            double rlat1 = Math.PI * lat1 / 180;
+            double rlat2 = Math.PI * lat2 / 180;
+            double theta = lon1 - lon2;
+            double rtheta = Math.PI * theta / 180;
+            double dist =
+                Math.Sin(rlat1) * Math.Sin(rlat2) + Math.Cos(rlat1) *
+                Math.Cos(rlat2) * Math.Cos(rtheta);
+            dist = Math.Acos(dist);
+            dist = dist * 180 / Math.PI;
+            dist = dist * 60 * 1.1515;
+
+            switch (unit)
+            {
+                case 'K': //Kilometers -> default
+                    return dist * 1.609344;
+                case 'N': //Nautical Miles 
+                    return dist * 0.8684;
+                case 'M': //Miles
+                    return dist;
+            }
+
+            return dist;
+        }
+
         public async Task<IActionResult> ApproveVisit(int id)
         {
             var clientRoute = _context.ClientRoute.Find(id);
+
+            var route_PlaceOfInterestTemp = _context.Route_PlaceOfInterest
+                .Where(p => p.Route_id == clientRoute.Route_id)
+                .Where(p => clientRoute.CurrentNumber == p.Number).FirstOrDefault();
+
+            var currentPlace = _context.PlaceOfInterest.Find(route_PlaceOfInterestTemp.PlaceOfInterest_id);
+
             clientRoute.CurrentNumber += 1;
 
             var nextPlace = _context.Route_PlaceOfInterest
                 .Where(p => p.Route_id == clientRoute.Route_id)
                 .Where(p => clientRoute.CurrentNumber == p.Number).FirstOrDefault();
 
+            var remoteIpAddress = Request.HttpContext.Connection.RemoteIpAddress;
+            var Ip_Api_Url = "http://ip-api.com/json/" + "88.222.14.81"; //remoteIpAddress.ToString();
+            string geoLocationInfo = "{ \"status\": \"fail\" }";
+            using (HttpClient httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                // Pass API address to get the Geolocation details 
+                httpClient.BaseAddress = new Uri(Ip_Api_Url);
+                HttpResponseMessage httpResponse = httpClient.GetAsync(Ip_Api_Url).GetAwaiter().GetResult();
+                // If API is success and receive the response, then get the location details
+                if (httpResponse.IsSuccessStatusCode)
+                {
+                    geoLocationInfo = httpResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+            }
+            var geoJson = JObject.Parse(geoLocationInfo);
+            double userLatitude = geoJson.SelectToken("lat").Value<double>();
+            double userLongitude = geoJson.SelectToken("lon").Value<double>();
+
+            var routePlaceCoords = currentPlace.Koordinates;
+            var placeCoords = routePlaceCoords.Split(',');
+            double placeLatitude = Double.Parse(placeCoords[0]);
+            double placeLongitude = Double.Parse(placeCoords[1]);
+
+            var distance = DistanceTo(userLatitude, userLongitude, placeLatitude, placeLongitude);
+            if (distance > 1)
+            {
+                TempData["ErrorMessage"] = "Esate per toli nuo objekto! " + " Esate nutole " + Math.Round(distance, 2).ToString() + " km nuo vietos!";
+                return RedirectToAction("PlaceOfInterestView", new { id = id });
+            }
+            else
+            {
+                TempData["SuccessMessage"] = "Aplankymas uzskaitytas! " + " Esate nutole " + Math.Round(distance, 2).ToString() + " km nuo vietos!";
+            }
+            
+
             if (nextPlace == null)
             {
+                clientRoute.CurrentNumber -= 1;
+                clientRoute.State_Id = 3;
+                try
+                {
+                    _context.ClientRoute.Update(clientRoute);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return NotFound();
+                    throw;
+                }
                 return RedirectToAction("OpenFavouriteRoutes");
             }
             try
@@ -143,6 +242,8 @@ namespace IS_Turizmas.Controllers
                 return NotFound();
                 throw;
             }
+
+            TempData["BANDOM"] = distance.ToString();
             return RedirectToAction("PlaceOfInterestView", new { id = id });
         }
         /*
